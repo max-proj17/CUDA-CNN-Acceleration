23f8f8218a1310eed3648da7edd49b8c1ccb47ab
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-11-08 15:36:04 -0600

    Successfully moved tensor to GPU!

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-// main.cpp
+#include <torch/torch.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, CUDA World!" << std::endl;
-    std::cin.get();  // Keep the console window open
+    // Create a tensor and print it.
+    torch::Tensor tensor = torch::rand({ 2, 3 });
+    std::cout << tensor << std::endl;
+
+    // Check if CUDA is available and move the tensor to GPU
+    if (torch::cuda::is_available()) {
+        tensor = tensor.to(torch::kCUDA);
+        std::cout << "CUDA is available! Tensor moved to GPU." << std::endl;
+    }
+    else {
+        std::cout << "CUDA is not available." << std::endl;
+    }
+    // Wait for user input to close the console window
+    std::cout << "Press ENTER to exit..." << std::endl;
+    std::cin.get();
     return 0;
 }
