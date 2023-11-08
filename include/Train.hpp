e125ee8966ba32778e516e6fc5504a925efd8bc3
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-11-08 17:10:52 -0600

    Fixed train function def in header file

--- include/Train.hpp
+++ include/Train.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <torch/torch.h>
+#include "Metrics.hpp"
 
-// Declare the train function with the required arguments
 void train(torch::nn::Module& model,
     torch::data::DataLoader<torch::data::datasets::MNIST>& data_loader,
     torch::optim::Optimizer& optimizer);
+
--- src/Train.cpp
+++ src/Train.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include <vector>
 
-void train(torch::nn::Module& model, torch::data::DataLoader<torch::data::datasets::MNIST>& data_loader, torch::optim::Optimizer& optimizer) {
+void train(Net& model, torch::data::DataLoader<torch::data::datasets::MNIST>& data_loader, torch::optim::Optimizer& optimizer) {
     model.train();
     std::vector<torch::Tensor> epoch_predictions, epoch_targets;
 
